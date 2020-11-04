6da369b0f94d9b29b1038b6d3214cb129241e447
samofalov <s.artiom@gmail.com>
2020-11-04 21:33:03 +0200

    Array is populated with elements

--- MyExample/src/application/Application.java
+++ MyExample/src/application/Application.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] arguments) {
 
+		int size = 100;
+		
+		if(!"".equals(arguments[0])) {
+			size = Integer.parseInt(arguments[0]);
+		}
+		
+		ArrayList<Double> numbers = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++) {
+			numbers.add(Math.random());
+		}
+		
+		Collections.sort(numbers);
+		
+		System.out.println(numbers);
 	}
 
 }
